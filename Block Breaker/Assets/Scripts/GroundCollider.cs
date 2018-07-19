47dee0764a4e8af1e63061ed4f83664387378b97
donkii <donatkajtazi@gmail.com>
2018-07-19 16:45:05 +0200

    Finished some screen and groundwork

---
+++ Block Breaker/Assets/Scripts/GroundCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GroundCollider : MonoBehaviour {
+
+    public LevelManager levelManager;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        levelManager.LoadLevel("Win screen");
+    }
+
+
+}
